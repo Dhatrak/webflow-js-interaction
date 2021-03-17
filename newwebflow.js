d6933cf8443c04e77045aaa0c46efa4ffbe1c0ef
Dhatrak <53165897+Dhatrak@users.noreply.github.com>
2021-03-17 10:15:59 +0530

    Create newwebflow.js

---
+++ newwebflow.js
@@ -0,0 +1,31 @@
+<script>
+       // $(document).ready(function () {
+         //   $(function () {
+           //     $('.reference-card-text-block h3').attr('id', 'color-theme');
+             //   $(".color-option, .reference-card-text-block #color-theme").each(function (index, element) {
+               //     var textColor = $(this).text();
+                 //   var parentBg = $(this).parent();                 
+
+                   // switch (textColor) {
+                     //   case "Hvit og purpur":
+                       //     parentBg.css("background", "#FFFFFF");
+                         //    parentBg.children().css("color", "#6c22ca");
+                           // break;
+
+                        //case "Mørk og teal":
+                          //  parentBg.css("background", "#001c27");
+                            //parentBg.children().css("color", "#00ded7");
+                            //break;
+
+                        //case "Rød og hvit":
+                          //  parentBg.css("background", "#FF414D");
+                           // parentBg.children().css("color", "#FFFFFF");
+                           // break;
+                            
+                       // default:
+                       //break;
+                    //}
+                //});
+           // });
+        //});
+    </script>
